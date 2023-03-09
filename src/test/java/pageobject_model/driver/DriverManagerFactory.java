2bc12d599dfa568d25cc2b173935316cd48128b0
Inna Orlova <inna_orlova@epam.com>
2023-03-09 12:48:54 +0200

    Commented rows were removed

--- src/test/java/pageobject_model/driver/DriverManagerFactory.java
+++ src/test/java/pageobject_model/driver/DriverManagerFactory.java
@@ -2,29 +2,11 @@ package pageobject_model.driver;
 
 public class DriverManagerFactory {
     private static  DriverManager driverManager;
-    private final static DriverManagerFactory factory = new DriverManagerFactory(); //final was removed
-
-//        public static DriverManager getManager() {
-//            DriverManager driverManager = null;
-//            switch (System.getProperty("browser")) {
-//                case "firefox":
-//                    driverManager = new FirefoxDriverManager();
-//                    break;
-//                default:
-//                    driverManager = new ChromeDriverManager();
-//                    break;
-//            }
-//            return driverManager;
-//        }
+    private final static DriverManagerFactory factory = new DriverManagerFactory();
 
     public static DriverManager getManager() {
-
-//        if(null == factory){
-//            factory = new DriverManagerFactory();
-//        }
         return driverManager;
     }
-
     private DriverManagerFactory() {
 
         switch (System.getProperty("browser")) {
